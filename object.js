c2587c9337f04fd9597529040458571ed1a68b40
mohammed mojeed <mohammedmojeed3@gmail.com>
2024-10-30 09:00:21 -0700

    commiting this the firsttime

---
+++ object.js
@@ -0,0 +1,48 @@
+let obj ={
+    name:'mohammed mojeed ramadan',
+    course:'masters in artificial intelligence',
+
+}
+
+obj.age =  33;
+console.log(obj)
+
+myObj = {//this is calld using object literal
+    name:"John",
+    age:30,
+    myCars: {
+      car1:"Ford",
+      car2:"BMW",
+      car3:"Fiat"
+    }
+  }
+
+  console.log(myObj.myCars)
+
+
+  //using object methods
+
+
+  let comp = new Object();
+  comp.name="mohammed";
+  comp.age ="27";
+
+  console.log(comp)
+
+  //usinfg the Object constructor function
+
+
+
+
+  function Person(firstname, secondname, lastname, age , eye){
+
+    this.firstName= firstname;
+    this.secondName =secondname;
+    this.lastName= lastname;
+    this.age=age;
+    this.eye=eye;
+  }
+
+  let mojeed= new Person('mohammed','mojeed','kashimawo','26')
+
+  console.log(mojeed.firstName)
