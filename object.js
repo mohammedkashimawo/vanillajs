9d431b7fb7d47dd3cedb2cd17a25e3228114ce3c
mohammed mojeed <mohammedmojeed3@gmail.com>
2024-11-02 06:35:53 -0700

    goof

--- object.js
+++ object.js
@@ -50,3 +50,32 @@ myObj = {//this is calld using object literal
 
 
   //    LOOPS USING JAVASCRIPT
+
+
+///lets work on lwitches with  this
+
+let abc= new Date()
+let obs= abc.getDay()
+console.log(obs)
+
+switch(obs){
+  case 0:
+    console.log('sunday');
+  case 1:
+  console.log('monday');
+  case 2:
+    console.log('teusday');
+  case 3:
+    console.log('wednesday');
+  case 4:
+    console.log('thrsday');
+  case 5:
+    console.log('friday');
+  case 6:
+    console.log('saturday');
+  
+    default:
+      console.log('your code not found')
+
+
+}
